197f5e565cb273605c471a2fc1409f8761129d8f
teddarific <teddyni19@gmail.com>
2023-09-29 11:01:36 -0700

    Init

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [tsconfigPaths(), react()],
+	build: {
+		lib: {
+			entry: resolve(__dirname, "src/index.ts"),
+			name: "ShadCdn",
+		},
+		rollupOptions: {
+			// make sure to externalize deps that shouldn't be bundled
+			// into your library
+			external: ["react"],
+			output: {
+				// Provide global variables to use in the UMD build
+				// for externalized deps
+				globals: {
+					react: "React",
+				},
+			},
+		},
+	},
+});
